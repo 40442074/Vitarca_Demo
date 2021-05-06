3c02a48ce31833cbe3b85b4f13188c19ca26fa30
Lawrence-B120 <59940948+Lawrence-B120@users.noreply.github.com>
2021-05-06 22:06:39 +0100

    tweaked camera sprite and colours
    camera cone changes colour when intersecting player, updated sprites and cone colour sprites

--- Coursework/components/cmp_camera.cpp
+++ Coursework/components/cmp_camera.cpp
@@ -6,44 +6,63 @@ using namespace std;
 
 void CameraComponent::update(double dt) {	//needs to sentry back and forth and then detect if comes into contact with player
 	static float angle = 0.f;
-	angle += 1.f * dt;
+	angle += 0.5f * dt;
 
 	//_parent->setRotation(0.f + sin(angle) * 45.f);
-	//_body->SetAngularVelocity(0.f + sin(angle) * 45.f);
-	_body->SetTransform(_body->GetPosition(), 0.f + sin(angle) /** 45.f*/);
-	_parent->setPosition(Physics::invert_height(Physics::bv2_to_sv2(_body->GetPosition())));
-	_parent->setRotation(-(180 / b2_pi) * _body->GetAngle());
-
-
-	//intersect code -- if player fixture intersects cone fixture. color::red
-	//b2Fixture playerBody;
-
-	if (b2TestOverlap(_playerBodyFixture->GetShape(), 0, _fixture->GetShape(), 0, _playerBodyFixture->GetBody()->GetTransform(), _body->GetTransform()))
+	if (_tag == "Vision")
 	{
-		cout << "player and camera overlap!!";
+		_body->SetTransform(_body->GetPosition(), 0.f + sin(angle) /** 45.f*/);
+		_parent->setPosition(Physics::invert_height(Physics::bv2_to_sv2(_body->GetPosition())));
+		_parent->setRotation(-(180 / b2_pi) * _body->GetAngle());
+
+		if (b2TestOverlap(_playerBodyFixture->GetShape(), 0, _fixture->GetShape(), 0, _playerBodyFixture->GetBody()->GetTransform(), _body->GetTransform()))
+		{
+			//cout << "player and camera overlap!!";
+			_colour.r = 255;
+			_colour.g = 0;
+		}
+		else
+		{
+			_colour.r = 0;
+			_colour.g = 255;
+		}
 	}
+	else if(_tag == "Top")
+	{
+		_parent->setRotation(0.f + sin(angle) * -45.f);
+	}
+
+}
+
+void CameraComponent::SetColour(sf::Color c) {
+	c.r = _colour.r;
+	c.g = _colour.g;
+	c.b = _colour.b;
+	
+};
+
+sf::Color CameraComponent::GetColour() {
+	return _colour;
 }
 
 b2Fixture* CameraComponent::getFixture() {
 	return _fixture;
 }
 
-CameraComponent::CameraComponent(Entity* p, b2Fixture *pb) : Component(p) {
+CameraComponent::CameraComponent(Entity* p, b2Fixture *pb, std::string tag) : Component(p) {
+
+	_tag = tag;
 
 	_playerBodyFixture = pb;
 
 	b2BodyDef BodyDef;
-	// Is Dynamic(moving), or static(Stationary)
 	BodyDef.type = b2_dynamicBody;
 	BodyDef.position = Physics::sv2_to_bv2(Physics::invert_height(p->getPosition()));
-
 	// Create the body
 	_body = Physics::GetWorld()->CreateBody(&BodyDef);
 	_body->SetActive(true);
-	//_direction = Vector2f(1.0f, 0);
-	//_speed = 100.0f;
-	//_sprite = sprite;
-	//Create leg space
+
+	//Create collision cone fixture
 	b2PolygonShape cone;
 	b2Vec2 points[4] = { Physics::sv2_to_bv2(Vector2f(22, 0), true), Physics::sv2_to_bv2(Vector2f(-22, 0), true), Physics::sv2_to_bv2(Vector2f(-100, -332), true),  Physics::sv2_to_bv2(Vector2f(100, -332), true) };
 	cone.Set(points, 4);
